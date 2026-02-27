77e05503c957276082800ac77611a1184ecad667
Saad Sayyed <saad.work.grove@gmail.com>
2026-02-27 16:49:12 +0530

    Nativewind

---
+++ mobile-app/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Index = () => {
+  return (
+    <View className="bg-red-500 flex-1 justify-center items-center">
+      <Text>Index</Text>
+    </View>
+  );
+};
+
+export default Index;
