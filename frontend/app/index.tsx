c5570884ddb7608976d358e2134267cebb59b1ab
Saad Sayyed <saad.work.grove@gmail.com>
2026-02-28 14:49:06 +0530

    Starting with User auth

---
+++ frontend/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Home = () => {
+  return (
+    <View className="flex-1 justify-center items-center w-full">
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
